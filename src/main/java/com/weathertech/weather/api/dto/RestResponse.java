1a768d5f2b6907e777a4f696ea6cc46b7bffdb5f
alenovich123@gmail.com <2783200Aa>
2021-09-06 16:27:36 +0500

    - unused code deleted

--- src/main/java/com/weathertech/weather/api/dto/RestResponse.java
+++ src/main/java/com/weathertech/weather/api/dto/RestResponse.java
@@ -8,24 +8,13 @@ public class RestResponse {
 
     private int error = 0;
     private String errMessage = null;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String errIconFileName = null;
     private Object data = null;
-    @JsonIgnore
-    private String provider = null;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String referenceId = null;
 
     public static final RestResponse ERROR_SERVICE_UNAVAILABLE = new RestResponse(500, "Service unavailable");
     public static final RestResponse ERROR_WRONG_PARAMETERS = new RestResponse(400, "Wrong parameters");
     public static final RestResponse ERROR_NOT_FOUND = new RestResponse(404, "Not found");
     public static final RestResponse ERROR_FORBIDDEN = new RestResponse(403, "Not allowed");
     public static final RestResponse OK = new RestResponse(0, "ok");
-
-    public RestResponse(int errorCode, String errorMessage, String provider, String iconFileName, JsonElement parse) {
-
-    }
 
     public RestResponse(int error, String errMessage)
     {
@@ -49,21 +38,6 @@ public class RestResponse {
         this.data = o;
         this.error = error;
         this.errMessage = errMessage;
-    }
-
-    public RestResponse(Object o, int error, String errMessage, String provider)
-    {
-        this.data = o;
-        this.error = error;
-        this.errMessage = errMessage;
-        this.provider = provider;
-    }
-
-    public RestResponse(int error, String errMessage, String provider)
-    {
-        this.error = error;
-        this.errMessage = errMessage;
-        this.provider = provider;
     }
 
     public Object getData() {
@@ -90,33 +64,5 @@ public class RestResponse {
         this.errMessage = errMessage;
     }
 
-    @Override
-    public String toString() {
-        return "RestError{" + "error=" + error + ", errMessage=" + errMessage + ", data=" + data + '}';
-    }
-
-    public String getProvider() {
-        return provider;
-    }
-
-    public void setProvider(String provider) {
-        this.provider = provider;
-    }
-
-    public String getErrIconFileName() {
-        return errIconFileName;
-    }
-
-    public void setErrIconFileName(String errIconFileName) {
-        this.errIconFileName = errIconFileName;
-    }
-
-    public String getReferenceId() {
-        return referenceId;
-    }
-
-    public void setReferenceId(String referenceId) {
-        this.referenceId = referenceId;
-    }
 
 }
